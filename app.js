a619de79e7763363bd79e19f2140927776ee73b4
sanidhyagupta-web <sanidhya.gupta@kickdrumtech.com>
2026-02-10 21:49:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get("/health", (req, res) => {
 });
 
 app.get("/api/v1/hello", (req, res) => {
-	res.json({ message: "Hello from Elastic Sanidhya Beanstalk!" });
+	res.json({ message: "Hello from Elastic Beanstalk!" });
 });
 
 app.post("/api/v1/echo", (req, res) => {
@@ -20,4 +20,5 @@ app.post("/api/v1/echo", (req, res) => {
 app.listen(port, "0.0.0.0", () => {
 	console.log(`API running on port ${port}`);
 });
+
 
